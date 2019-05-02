8735bf65d45bdf64b2b7ef71e117cf2b406dc602
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-02 15:45:43 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,23 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def index
+    render json: Game.all
   end
 
   def create
+    game = Game.new(:state => params[:state])
+    game.save
+    render json: game
   end
 
   def update
+    game = Game.find(params[:id])
+    game.update(:state => params[:state])
+    render json: game
   end
 
   def show
-    render json_api: Game.find(params[:id])
+    render json: Game.find(params[:id])
   end
 
 end
