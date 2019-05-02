c2913262836c5c6e129dc4781298bf1d6eba5dd3
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-02 14:56:57 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,16 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def show
+    render json_api: Game.find(params[:id])
+  end
 
 end
---
+++ app/serializers/game_serializer.rb
@@ -0,0 +1,3 @@
+class GameSerializer < ActiveModel::Serializer
+  attributes :state
+end
